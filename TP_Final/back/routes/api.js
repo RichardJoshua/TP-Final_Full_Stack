7aec5b02e6e6cdc5b7ebaba5ef68d768e3d8c919
Ricardo <joshua_knra@yahoo.com.ar>
2023-01-29 08:52:11 -0300

    add correo

--- TP_Final/back/routes/api.js
+++ TP_Final/back/routes/api.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var novedadesModel = require('./../models/novedadesModel');
 var cloudinary = require('cloudinary').v2;
 
+var nodemailer = require('nodemailer');
+
 router.get('/novedades', async function(req, res, next) {
     let novedades = await novedadesModel.getNovedades();
  
@@ -11,7 +13,7 @@ router.get('/novedades', async function(req, res, next) {
       if (novedades.img_id) {
         const imagen = cloudinary.url(novedades.img_id, {
           width: 960,
-          height:200,
+          height:400,
           crop: 'fill'
         });
         return {
@@ -29,4 +31,32 @@ router.get('/novedades', async function(req, res, next) {
     res.json(novedades);
   });
 
+  router.post('/contacto',async (req, res) => {
+
+    // console.log(req.body) //Trae datos?
+    const mail = {
+      to: 'esp8266.joshua@gmail.com',
+      subject: 'Contacto desde la Web',
+      html: `${req.body.nombre} se contacto a traves y quiere mas info a este correo :${req.body.email} <br> 
+      Ademas, hizo el siguiente comentario: ${req.body.mensaje} <br> Su tel es: ${req.body.telefono}`
+
+    }
+    
+    const transport = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASS
+        }
+      }); //cierre transporter
+
+      await transporter.sendMail(mail);
+
+      res.status(201).json({
+        error:false,
+        message:'Mensaje enviado correctamente',
+    });
+  });
+  
   module.exports = router;
